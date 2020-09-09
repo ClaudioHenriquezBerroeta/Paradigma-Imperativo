cc77302454ea05abe32fb8b7c6d718b450096f9c
Claudio Henríquez <63313873+ClaudioHenriquezBerroeta@users.noreply.github.com>
2020-09-09 16:50:20 -0300

    update2

--- ejercicio3.c
+++ ejercicio3.c
@@ -1,8 +1,19 @@
 #include <stdio.h>
+#include <stdlib.h>
+//%i entero (int)
+//%c caracter (char)
+//%f real (float)
+//%s cadena caracteres (char *)
 
 int main(){
 	int a;
-	printf("Hola mundo\b \n");
-	scanf("%i",&a);
+	char b;
+	float c;
+	char *d= malloc(sizeof(char)*10);
+
+	scanf("%s",d);
+
+	printf("El número ingresado es: %s \n",d);
+
 	return 0;
 }
