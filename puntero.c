5df232659966840ddefb39ca5907cd43f5e07799
Claudio Henríquez <63313873+ClaudioHenriquezBerroeta@users.noreply.github.com>
2020-09-21 16:59:05 -0300

    update

--- puntero.c
+++ puntero.c
@@ -1,16 +1,34 @@
 #include <stdio.h>
 #include <stdlib.h>
+int suma;
+
+int sumador_normal(int a,int b){
+	suma=a+b;
+	return a+b;
+}
+
+void sumador_puntero(int *a, int b){
+	suma=*a+b;
+	*a=*a+b;
+}
+
+
 int main(int argc, char const *argv[])
 {
 	int *var_puntero;
 	int var_normal;
+	int suma_normal;
+	int suma_puntero;
 
-	var_puntero=malloc(sizeof(int));
+	var_puntero=malloc(sizeof(int)*100);
 	
 	var_normal=10;
 	*var_puntero=20;
-	printf("%i\n", var_normal);
-	printf("%i\n", *var_puntero);
+
+	suma_normal = sumador_normal(var_normal,*var_puntero);
+	printf("%i\n", suma_normal);
+	sumador_puntero(var_puntero,var_normal);
+	printf("%i\n", suma);
 	free(var_puntero);
 	return 0;
 }
