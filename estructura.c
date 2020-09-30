ef41432ca7804fd67ca2140e3eefe7ae2c040053
Claudio Henríquez <63313873+ClaudioHenriquezBerroeta@users.noreply.github.com>
2020-09-30 16:15:40 -0300

    new

---
+++ estructura.c
@@ -0,0 +1,51 @@
+
+#include <stdio.h>
+#include <stdlib.h>
+
+#define n 10
+#define letra 65
+
+struct nombre{
+	int variable_1;
+	int variable_n[n];
+};
+typedef struct nombre estructura1;
+
+void sumatoria(int *numeros,int *resultado){
+	int largo = length(numeros);
+	int i;
+	*resultado=0;
+	for(i=0;i<largo;i++){
+		*resultado=*resultado+numeros[i];
+	}
+}
+
+int main(){
+	int *arreglo_enteros;
+	char *arreglo_caracteres;
+	int i;
+	estructura1 arreglo[10];
+	arreglo[0].variable_1=1;
+	arreglo[0].variable_n[0]=1;
+
+	arreglo_enteros=malloc(sizeof(int)*n);
+	arreglo_caracteres=malloc(sizeof(char)*n);
+
+	for(i=0;i<n;i++){
+		arreglo_enteros[i]=i;
+	}
+	for(i=0;i<n;i++){
+		printf("%i-",arreglo_enteros[i]);
+	}
+	printf("\b \n");
+	for(i=letra;i<(letra+n);i++){
+		arreglo_caracteres[i-letra]=i;
+	}
+	for(i=0;i<(n);i++){
+		printf("%c-",arreglo_caracteres[i]);
+	}
+	printf("\b \n");
+	free(arreglo_enteros);
+	free(arreglo_caracteres);
+	return 0;
+}
